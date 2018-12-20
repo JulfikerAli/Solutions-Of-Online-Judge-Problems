dc15ce439b5bb923297017c3f2b98b955e809ec9
Julfiker Ali <julfikercseru@gmail.com>
2018-12-20 21:41:41 +0600

    The Problem Has Been Solved

--- UriBeginners/src/uribeginners/SimpleProduct.java
+++ UriBeginners/src/uribeginners/SimpleProduct.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uribeginners;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Julfiker
- */
 public class SimpleProduct {
+  
     public static void main(String[] args) {
 
      int A,B,X;
