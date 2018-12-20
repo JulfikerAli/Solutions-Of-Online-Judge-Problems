7e66934008b57f7a3a48cce857debb9e49a7d956
Julfiker Ali <julfikercseru@gmail.com>
2018-12-20 21:25:36 +0600

    The Problem Has Been Solved

---
+++ UriBeginners/src/uribeginners/SimpleSum.java
@@ -0,0 +1,15 @@
+package uribeginners;
+import java.util.Scanner;
+
+public class SimpleSum {
+public static void main(String[] args) {
+
+     int A,B,SOMA;
+      Scanner sum = new Scanner(System.in);
+      A = sum.nextInt();
+      B = sum.nextInt();
+      SOMA=A+B;
+      System.out.print("SOMA = "+SOMA+"\n"); 
+
+   }
+}
