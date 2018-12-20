ede9184d2ed7f2a5504da154711526b8fc4e6c0c
Julfiker Ali <julfikercseru@gmail.com>
2018-12-20 21:06:42 +0600

    The Problem Has Been Solved

---
+++ UriBeginners/src/uribeginners/ExtremelyBasic.java
@@ -0,0 +1,17 @@
+package uribeginners;
+import java.util.Scanner;
+public class ExtremelyBasic {
+ 
+
+ public static void main(String[] args) {
+
+      int A, B, X;
+      Scanner sum = new Scanner(System.in);
+      A = sum.nextInt();                     //take input for A
+      B = sum.nextInt();                    //take input for  B
+      X = A + B;                           //Basic summation X = A + B
+      System.out.print("X = "+X+"\n");    //Hardly advised to give \n at last
+
+   }
+
+}
